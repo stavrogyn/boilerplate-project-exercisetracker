a65d35b91991b12ece5d1b241b3db9233dad65df
Stavrogyn <nikita.krugovoy@aviasales.ru>
2021-02-22 14:33:28 +0300

    Delete excess files

--- router.js
+++ router.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const controller = require('./controller')
 const bodyParser = require('body-parser');
 
-const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', controller.sendMainPage);
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const router = require('./router')
 const app = express()
 const port = process.env.PORT || 2000
 require('dotenv').config()
 
-app.use(cors())
+
 app.use(express.static('public'))
 app.use(router)
 
